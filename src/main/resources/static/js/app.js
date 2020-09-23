0f0f767ff941db574ffc963cd7f40a247f69fe6c
santiago <srubianof>
2020-09-22 23:54:22 -0500

    haciendo canvas

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,53 @@
-var app = (function (){
-    var
-});
+var app = (function () {
+    var nombreCine = "";
+    var fechaFuncion = "";
+    var listaFunciones = [];
+
+
+    var mapObjetos = (funciones) => {
+        listaFunciones = funciones.map(({movie: {name, genre}, date}) => ({
+                name: name,
+                genre: genre,
+                time: date
+            })
+        )
+        $("#tablaMovies > tbody").empty();
+        listaFunciones.forEach(({name, genre, time}) => {
+            $("#tablaMovies > tbody").append(
+                "<tr>" +
+                " <td>" + name + "</td>" +
+                " <td>" + genre + "</td>" +
+                " <td>" + time + "</td>" +
+                "</tr>"
+            );
+        });
+    }
+    var dibujarObjetos = function () {
+        var canvas = document.getElementById("myCanvas");
+        var lapiz = canvas.getContext("2d");
+        lapiz.fillRect(0, 0, canvas.width, canvas.height);
+        lapiz.beginPath();
+        lapiz.moveTo(12,12);
+    }
+
+
+    return {
+        dibujarObjetos(nombre, fecha) {
+            apimock.getFunctionsByCinemaAndDate(nombre, fecha, dibujarObjetos)
+        },
+        actualizarListadodeFunciones(nombre, fecha) {
+            this.cambiarFecha(fecha);
+            this.cambiarNombreCine(nombre);
+            apimock.getFunctionsByCinemaAndDate(nombre, fecha, mapObjetos);
+        },
+        consultarAsientosDisponibles(nombreCine, fecha, nombrePelicula) {
+            apimock.getFunctionsByCinemaAndDate(nombreCine, fecha, dibujarObjetos);
+        },
+        cambiarNombreCine(nombre) {
+            nombreCine = nombre;
+        },
+        cambiarFecha(fecha) {
+            fechaFuncion = fecha;
+        }
+    }
+})();
