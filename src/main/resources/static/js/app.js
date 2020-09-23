58baac4974ad043fa826a1ae9d203a43070ae304
santiago <srubianof>
2020-09-23 02:44:58 -0500

    casi listo :3

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,53 +1,75 @@
 var app = (function () {
-    var nombreCine = "";
-    var fechaFuncion = "";
-    var listaFunciones = [];
+        var nombreCine = "";
+        var fechaFuncion = "";
+        var listaFunciones = [];
+        var listaSillas = [];
 
 
-    var mapObjetos = (funciones) => {
-        listaFunciones = funciones.map(({movie: {name, genre}, date}) => ({
-                name: name,
-                genre: genre,
-                time: date
-            })
-        )
-        $("#tablaMovies > tbody").empty();
-        listaFunciones.forEach(({name, genre, time}) => {
-            $("#tablaMovies > tbody").append(
-                "<tr>" +
-                " <td>" + name + "</td>" +
-                " <td>" + genre + "</td>" +
-                " <td>" + time + "</td>" +
-                "</tr>"
-            );
-        });
-    }
-    var dibujarObjetos = function () {
-        var canvas = document.getElementById("myCanvas");
-        var lapiz = canvas.getContext("2d");
-        lapiz.fillRect(0, 0, canvas.width, canvas.height);
-        lapiz.beginPath();
-        lapiz.moveTo(12,12);
-    }
+        var mapObjetos = (funciones) => {
+            listaFunciones = funciones.map(({movie: {name, genre}, date}) => ({
+                    name: name,
+                    genre: genre,
+                    time: date.split(" ")[1],
+                    date: date
+                })
+            )
+            $("#tablaMovies > tbody").empty();
+            listaFunciones.forEach(({name, genre, time, date}) => {
+                $("#tablaMovies > tbody").append(
+                    `<tr>
+                    <td> ${name} </td>
+                    <td> ${genre} </td>
+                    <td> ${time} </td>
+                    <td> <button type="button" class="btn btn-success btn-lg btn-block" onclick="app.dibujarObjetos($('#cinemaName').val(),'${date}','${name}')">Ver Sillas</button> </td>
+                    </tr>`
+                );
+            });
+        }
+        var dibujarObjetos = function (sillitas) {
+            var canvas = document.getElementById("myCanvas");
+            var lapiz = canvas.getContext("2d");
+            console.log(sillitas);
+            lapiz.strokeStyle = 'lightgrey';
+            for (let i = 0; i < 7; i++) {
+                for (let j = 0; j < 12; j++) {
+                    if (sillitas[i][j] === true) {
+                        lapiz.fillStyle = "#FFC300";
+                    } else {
+                        lapiz.fillStyle = "#900C3F";
+                    }
+                    lapiz.fillRect(j * 85, i * 85, 80, 80);
+                }
+            }
+        }
 
-
-    return {
-        dibujarObjetos(nombre, fecha) {
-            apimock.getFunctionsByCinemaAndDate(nombre, fecha, dibujarObjetos)
-        },
-        actualizarListadodeFunciones(nombre, fecha) {
-            this.cambiarFecha(fecha);
-            this.cambiarNombreCine(nombre);
-            apimock.getFunctionsByCinemaAndDate(nombre, fecha, mapObjetos);
-        },
-        consultarAsientosDisponibles(nombreCine, fecha, nombrePelicula) {
-            apimock.getFunctionsByCinemaAndDate(nombreCine, fecha, dibujarObjetos);
-        },
-        cambiarNombreCine(nombre) {
-            nombreCine = nombre;
-        },
-        cambiarFecha(fecha) {
-            fechaFuncion = fecha;
+        return {
+            dibujarObjetos(nombre, fecha, nombrePelicula) {
+                console.log(fecha)
+                console.log(nombrePelicula)
+                apimock.getFunctionsByCinemaAndDate(nombre, fecha, (funciones) => {
+                    for (const funcion of funciones) {
+                        if (funcion.movie.name === nombrePelicula) {
+                            dibujarObjetos(funcion.seats);
+                            break;
+                            //:3
+                        }
+                    }
+                })
+            },
+            actualizarListadodeFunciones(nombre, fecha) {
+                this.cambiarFecha(fecha);
+                this.cambiarNombreCine(nombre);
+                apimock.getFunctionsByCinemaAndDate(nombre, fecha, mapObjetos);
+            },
+            consultarAsientosDisponibles(nombreCine, fecha, nombrePelicula) {
+                apimock.getFunctionsByCinemaAndDate(nombreCine, fecha, dibujarObjetos);
+            },
+            cambiarNombreCine(nombre) {
+                nombreCine = nombre;
+            },
+            cambiarFecha(fecha) {
+                fechaFuncion = fecha;
+            }
         }
     }
-})();
+)();
